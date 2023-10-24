e4a35f0ed06b36f38d77e5581e65bed99ed33f3a
hannes2709 <hannes.gubler@epfl.ch>
2023-10-24 21:13:36 +0200

    fixed problem with 0 and 1 quantile

--- federated_quantiles.py
+++ federated_quantiles.py
@@ -7,26 +7,32 @@ class FederatedPrivateQuantiles:
         self.x = x # vector of data points
         self.n = n # total number of data points
         self.z = [0] * len(q) # vector of quantiles
+        self.left_points = []
 
     def middlepoint(self, x_min, x_max, q_prime):
+        if len(q_prime) == 0:
+            return
+        if q_prime[0] == 0:
+            self.z[0] = self.x.min()
+            q_prime = q_prime[1:]
+        if q_prime[-1] == 1:
+            self.z[-1] = self.x.max()
+            q_prime = q_prime[:-1]
         mid = (x_min + x_max) / 2
         left = np.count_nonzero(self.x < mid)
+        self.left_points.append(mid)
         a = left / self.n
 
         if len(q_prime) == 1:
-            right = np.count_nonzero(self.x >= mid)  # Replace with your value or calculation
-            b = 1 - right / self.n
-
-            if a <= q_prime[0] <= b:
+            if a == q_prime[0]:
                 self.z[np.where(q == q_prime[0])[0][0]] = mid
-                self.z
                 return
 
-            if q_prime[0] <= a:
+            if q_prime[0] < a:
                 self.middlepoint(x_min, mid, q_prime)
                 return
 
-            if q_prime[0] >= b:
+            if q_prime[0] >= a:
                 self.middlepoint(mid, x_max, q_prime)
                 return
 
@@ -47,7 +53,7 @@ class FederatedPrivateQuantiles:
         return self.z
 
 # Example usage:
-q = np.array([0.1, 0.2, 0.3, 0.5, 0.66, 0.8, 0.95])
+q = np.array([0, 0.1, 0.2, 0.3, 0.5, 0.66, 0.8, 0.95, 1])
 # create x vector with numbers from 1 to 100
 x = np.array([i for i in range(1, 101)])
 n = x.shape[0]
@@ -55,3 +61,4 @@ n = x.shape[0]
 quantile_calculator = FederatedPrivateQuantiles(q, x, n)
 result = quantile_calculator.compute_quantiles()
 print(result)
+print(quantile_calculator.left_points)
