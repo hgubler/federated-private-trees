ed330ab8a5aa9f47122095bd9e1d02c74166a997
hannes2709 <hannes.gubler@epfl.ch>
2023-10-31 18:31:36 +0100

    Add exponential mechanism and plot results

--- federated_private_decision_tree.py
+++ federated_private_decision_tree.py
@@ -2,103 +2,146 @@ import numpy as np
 import federated_private_quantiles as fpq
 
 class FederatedPrivateDecisionTree:
-    def __init__(self, max_depth=5, min_samples_split=2, num_quantiles=10, privacy_budget=1):
+    def __init__(self, max_depth=5, min_samples_split=2, num_quantiles=10, privacy_budget=1, diff_privacy = True, leaf_mechanism='laplace'):
         self.max_depth = max_depth
         self.min_samples_split = min_samples_split
         self.num_quantiles = num_quantiles
         self.privacy_budget = privacy_budget
+        self.diff_privacy = diff_privacy
+        self.leaf_mechanism = leaf_mechanism
         self.tree = {}
         self.quantiles = {}
 
-    def entropy(self, p):
+    def __entropy(self, p):
+        if p == 0 or p == 1:
+            return 0
         return -p * np.log2(p)
     
-    def distribute_privacy_budget(self, epsilon):
-        self.quantile_epsilon = 0.5 * epsilon / ((self.max_depth + 1) * self.num_quantiles)
-        self.node_epsilon = 0.5 * epsilon / (self.max_depth + 1)
-        self.leaf_epsilon = 0.5 * epsilon
+    def __distribute_privacy_budget(self):
+        self.quantile_epsilon = 0.5 * self.privacy_budget / ((self.max_depth + 1) * self.num_quantiles)
+        self.node_epsilon = 0.5 * self.privacy_budget / (self.max_depth + 1)
+        self.leaf_epsilon = 0.5 * self.privacy_budget
+        return 
 
-    def fit(self, X, y):
+    def __quantile_calculator(self, X):
         n_samples, n_features = X.shape
-        # distribute privacy budget
-        self.distribute_privacy_budget(self.privacy_budget)
-        # define all possible quantile probabilities
+        # Define all possible quantile probabilities
         quantile_probabilities = np.array([i / n_samples for i in range(1, n_samples)])
-        # select num_quantiles quantile probabilities from the possible quantile probabilities in an equidistant manner
+        # Select num_quantiles quantile probabilities from the possible quantile probabilities in an equidistant manner
         quantile_probabilities = quantile_probabilities[::int(n_samples / self.num_quantiles)]
-        self.quantiles = {}
         for feature in range(n_features):
             self.quantiles[feature] = fpq.FederatedPrivateQuantiles(quantile_probabilities, X[:, feature], n_samples)
             self.quantiles[feature] = self.quantiles[feature].compute_quantiles().z
-        self.tree = self._grow_tree(X, y, quantiles=self.quantiles)
-
-    def _grow_tree(self, X, y, depth=0, quantiles=None):
-        n_samples, n_features = X.shape
-        n_labels = len(np.unique(y))
-
-        # Check termination conditions
-        if (depth >= self.max_depth or n_samples < self.min_samples_split or n_labels == 1):
-            bincounts = np.bincount(y)
-            # add laplace noise to the bincounts
-            bincounts = np.array([np.random.laplace(x, 1 / self.leaf_epsilon) for x in bincounts])
+        return 
+    
+    def __create_leaf(self, y):
+        bincounts = np.bincount(y)
+        if (self.diff_privacy == False):
             leaf_value = int(np.argmax(bincounts))
             return leaf_value
+        if (self.diff_privacy == True):
+            # Add laplace noise to the bincounts
+            if (self.leaf_mechanism == 'laplace'):
+                bincounts = np.array([np.random.laplace(x, 1 / self.leaf_epsilon) for x in bincounts])
+                leaf_value = int(np.argmax(bincounts))
+                return leaf_value
+            if (self.leaf_mechanism == 'exponential'):
+                # Use number of samples per class as score for each class for the exponential mechanism
+                scores = bincounts
+                # sensitivity is 1 as removing/adding one datapoint changes result by 1
+                sensitivity = 1
+                probabilities = np.exp(self.leaf_epsilon * scores / (2 * sensitivity))
+                probabilities = probabilities / np.sum(probabilities)
+                leaf_value = int(np.random.choice(np.arange(len(bincounts)), p=probabilities))
+                return leaf_value
 
+            
+    
+
+    def __quantile_binning(self, X, feature):
+        # Bin the feature into a histogram according to the quantiles and store the bin counts in each bin
+        feature_binned = np.digitize(X[:, feature], bins=self.quantiles[feature])
+        feature_bincounts = np.bincount(feature_binned)
+        if (self.diff_privacy == False):
+            return feature_bincounts
+        if (self.diff_privacy == True):
+            # Add laplace noise to each bincount of the histogram
+            feature_bincounts = np.array([np.random.laplace(x, 1 / self.node_epsilon) for x in feature_bincounts])
+            return feature_bincounts
+    
+    def __best_split(self, X, y):
+        n_samples, n_features = X.shape
         best_entropy = np.inf
         best_feature = None
         best_threshold = None
-
-        if quantiles is None:
-            quantiles = self.quantiles
         for feature in range(n_features):
-            # bin the feature into a histogram according to the quantiles and store the bin counts in each bin
-            feature_binned = np.digitize(X[:, feature], bins=quantiles[feature])
-            feature_bincounts = np.bincount(feature_binned)
-            # add laplace noise to each bincount of the histogram
-            feature_bincounts = np.array([np.random.laplace(x, 1 / self.node_epsilon) for x in feature_bincounts])
-            for threshold in quantiles[feature]:
-                # calculate y_left by summing up the bincounts of the bins that are left and round to closest integer
-                left = round(np.sum(feature_bincounts[:np.digitize(threshold, bins=quantiles[feature])]))
-                # change left to 1 if it is smaller than 1
-                if left <= 0:
-                    # go to next iteration
+            # Bin the feature according to the quantiles
+            feature_bincounts = self.__quantile_binning(X, feature)
+            for threshold in self.quantiles[feature]:
+                # Calculate how many points are left of the threshold using the binned feature and round to closest integer
+                left = round(np.sum(feature_bincounts[:np.digitize(threshold, bins=self.quantiles[feature])]))
+                if left <= 0: # Go to next iteration, no split here (all points are on the right side of the threshold)
                     continue
-                right = round(np.sum(feature_bincounts[np.digitize(threshold, quantiles[feature]):]))
-                # change right to 1 if it is smaller than 1, no split here
-                if right <= 0:
-                    # go to next iteration
+                right = round(np.sum(feature_bincounts[np.digitize(threshold, self.quantiles[feature]):]))
+                if right <= 0: # Go to next iteration, no split here (all points are on the left side of the threshold)
                     continue
-                #y_left = y[X[:, feature] < threshold]
-                #y_right = y[X[:, feature] >= threshold]
-                # extract the smallest left elements from X[:, feature] and count how many of them are smaller than threshold
+
+                # Calculate the proportions of the labels in the left and right split
                 p_left = sum(y[X[:, feature] < threshold]) / left
                 p_right = sum(y[X[:, feature] >= threshold]) / right
+                # Adjust proportions to be in [0, 1] (due to noise added to bincounts)
                 if p_left > 1:
                     p_left = 1
                 if p_right > 1:
                     p_right = 1
-                entropy = (left/n_samples)*self.entropy(p_left) + (right/n_samples)*self.entropy(p_right)
+
+                # Calculate entropy of the splitted space (want to minimize entropy to maximize impurity decrease)
+                entropy = (left/n_samples)*self.__entropy(p_left) + (right/n_samples)*self.__entropy(p_right)
+
+                # If all points are on one side of the threshold, don't consider this split (maybe add a different solution later with min_leaf_size) and directly 
+                # create a leaf when this happens (in the grow_tree function)
+                if np.sum(X[:, feature] < threshold) == 0 or np.sum(X[:, feature] >= threshold) == 0:
+                    continue
+
                 if entropy < best_entropy:
                     best_entropy = entropy
                     best_feature = feature
-                    best_threshold = threshold
-        # if didnt find a split that reduces entropy, make a leaf node
+                    best_threshold = threshold  
+        return best_entropy, best_feature, best_threshold
+
+
+    def fit(self, X, y):
+        if (self.diff_privacy == True):
+            # Distribute privacy budget
+            self.__distribute_privacy_budget()
+        # Calculate quantiles
+        self.__quantile_calculator(X)
+        # Grow tree
+        self.tree = self.__grow_tree(X, y, depth=0)
+
+    def __grow_tree(self, X, y, depth=0):
+        n_samples, n_features = X.shape
+        n_labels = len(np.unique(y))
+        # Check termination conditions to potentially create a leaf
+        if (depth >= self.max_depth or n_samples < self.min_samples_split or n_labels == 1):
+            return self.__create_leaf(y)
+        
+        # Find best split
+        best_entropy, best_feature, best_threshold = self.__best_split(X, y)
+
+        # If didn't find a split that reduces entropy, make a leaf node
         if best_entropy == np.inf:
-            # make a leaf node
-            bincounts = np.bincount(y)
-            # add laplace noise to the bincounts
-            bincounts = np.array([np.random.laplace(x, 1 / self.leaf_epsilon) for x in bincounts])
-            leaf_value = int(np.argmax(bincounts))
-            return leaf_value
-        # Otherwise create sub-trees
+            return self.__create_leaf(y)
+        
+        # Otherwise divide space to create sub-trees
         left_idxs = X[:, best_feature] < best_threshold
         right_idxs = X[:, best_feature] >= best_threshold
 
         # Grow subtrees
-        left_tree = self._grow_tree(X[left_idxs], y[left_idxs], depth+1, quantiles)
-        right_tree = self._grow_tree(X[right_idxs], y[right_idxs], depth+1, quantiles)
+        left_tree = self.__grow_tree(X[left_idxs], y[left_idxs], depth+1)
+        right_tree = self.__grow_tree(X[right_idxs], y[right_idxs], depth+1)
 
-        # Create node dictionary
+        # Create node 
         node = {'feature': best_feature,
                 'threshold': best_threshold,
                 'left': left_tree,
--- tree_test.py
+++ tree_test.py
@@ -1,4 +1,6 @@
 from federated_private_decision_tree import FederatedPrivateDecisionTree
+from sklearn.metrics import accuracy_score
+from matplotlib import pyplot as plt
 # import breast cancer dataset from sklearn
 from sklearn.datasets import load_breast_cancer
 data = load_breast_cancer()
@@ -7,21 +9,111 @@ y = data.target
 # create train test split
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y)
-# create and fit model
-model = FederatedPrivateDecisionTree(max_depth=10, privacy_budget=0.5)
-model.fit(X_train, y_train)
-# make predictions
-predictions = model.predict(X_test)
-# calculate accuracy
-from sklearn.metrics import accuracy_score
-accuracy = accuracy_score(y_test, predictions)
-print("Accuracy: " + str(accuracy))
 
-# compare with decision tree from sklearn
-from sklearn.tree import DecisionTreeClassifier
-model = DecisionTreeClassifier(max_depth=10, criterion='entropy')
-model.fit(X_train, y_train)
-predictions = model.predict(X_test)
-accuracy = accuracy_score(y_test, predictions)
-print("Accuracy: " + str(accuracy))
+
+# create and fit model with exponential leaf mechanism
+exp_model = FederatedPrivateDecisionTree(max_depth=10, privacy_budget=1, diff_privacy=True, leaf_mechanism='exponential')
+exp_model.fit(X_train, y_train)
+# make predictions
+predictions = exp_model.predict(X_test)
+# calculate accuracy
+exp_accuracy = accuracy_score(y_test, predictions)
+print("Accuracy with exponential leaf labelling: " + str(exp_accuracy))
+
+# create and fit model with laplace leaf mechanism
+laplace_model = FederatedPrivateDecisionTree(max_depth=10, privacy_budget=1, diff_privacy=True, leaf_mechanism='laplace')
+laplace_model.fit(X_train, y_train)
+# make predictions
+predictions = laplace_model.predict(X_test)
+# calculate accuracy
+laplace_accuracy = accuracy_score(y_test, predictions)
+print("Accuracy with laplace leaf labelling: " + str(laplace_accuracy))
+
+# create and fit model without differential privacy
+no_dp_model = FederatedPrivateDecisionTree(max_depth=10, privacy_budget=1, diff_privacy=False)
+no_dp_model.fit(X_train, y_train)
+# make predictions
+predictions = no_dp_model.predict(X_test)
+# calculate accuracy
+no_dp_accuracy = accuracy_score(y_test, predictions)
+print("Accuracy without differential privacy: " + str(no_dp_accuracy))
+
+
+# plot accuracies of all three models vs depth on x-axis
+depths = [i for i in range(1, 20)]
+exp_accuracies = []
+laplace_accuracies = []
+no_dp_accuracies = []
+for depth in depths:
+    exp_model = FederatedPrivateDecisionTree(max_depth=depth, privacy_budget=1, diff_privacy=True, leaf_mechanism='exponential')
+    exp_model.fit(X_train, y_train)
+    predictions = exp_model.predict(X_test)
+    exp_accuracy = accuracy_score(y_test, predictions)
+    exp_accuracies.append(exp_accuracy)
+
+    laplace_model = FederatedPrivateDecisionTree(max_depth=depth, privacy_budget=1, diff_privacy=True, leaf_mechanism='laplace')
+    laplace_model.fit(X_train, y_train)
+    predictions = laplace_model.predict(X_test)
+    laplace_accuracy = accuracy_score(y_test, predictions)
+    laplace_accuracies.append(laplace_accuracy)
+
+    no_dp_model = FederatedPrivateDecisionTree(max_depth=depth, privacy_budget=1, diff_privacy=False)
+    no_dp_model.fit(X_train, y_train)
+    predictions = no_dp_model.predict(X_test)
+    no_dp_accuracy = accuracy_score(y_test, predictions)
+    no_dp_accuracies.append(no_dp_accuracy)
+
+
+plt.plot(depths, exp_accuracies, '-', label='exponential')
+plt.plot(depths, laplace_accuracies, '-', label='laplace')
+plt.plot(depths, no_dp_accuracies, '-', label='no differential privacy')
+plt.title("Accuracy vs depth")
+plt.xlabel("depth")
+plt.ylabel("accuracy")
+plt.legend()
+plt.show()
+# safe plot in plots folder
+plt.savefig("plots/accuracy_vs_depth.png")
+
+
+# repeat the same but now take privacy budged on x-axis
+privacy_budgets = np.linspace(0.1, 2, 10)
+exp_accuracies = []
+laplace_accuracies = []
+no_dp_accuracies = []
+for privacy_budget in privacy_budgets:
+    exp_model = FederatedPrivateDecisionTree(max_depth=10, privacy_budget=privacy_budget, diff_privacy=True, leaf_mechanism='exponential')
+    exp_model.fit(X_train, y_train)
+    predictions = exp_model.predict(X_test)
+    exp_accuracy = accuracy_score(y_test, predictions)
+    exp_accuracies.append(exp_accuracy)
+
+    laplace_model = FederatedPrivateDecisionTree(max_depth=10, privacy_budget=privacy_budget, diff_privacy=True, leaf_mechanism='laplace')
+    laplace_model.fit(X_train, y_train)
+    predictions = laplace_model.predict(X_test)
+    laplace_accuracy = accuracy_score(y_test, predictions)
+    laplace_accuracies.append(laplace_accuracy)
+
+    no_dp_model = FederatedPrivateDecisionTree(max_depth=10, privacy_budget=privacy_budget, diff_privacy=False)
+    no_dp_model.fit(X_train, y_train)
+    predictions = no_dp_model.predict(X_test)
+    no_dp_accuracy = accuracy_score(y_test, predictions)
+    no_dp_accuracies.append(no_dp_accuracy)
+
+plt.plot(privacy_budgets, exp_accuracies, '-', label='exponential')
+plt.plot(privacy_budgets, laplace_accuracies, '-', label='laplace')
+plt.plot(privacy_budgets, no_dp_accuracies, '-', label='no differential privacy')
+plt.title("Accuracy vs privacy budget")
+plt.xlabel("privacy budget")
+plt.ylabel("accuracy")
+plt.legend()
+plt.show()
+# safe plot in plots folder
+plt.savefig("plots/accuracy_vs_privacy_budget.png")
+
+
+
+
+
+
 
