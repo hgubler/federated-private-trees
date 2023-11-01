49fc76e263c82d8bb648dab0de48974b9da59e56
hannes2709 <hannes.gubler@epfl.ch>
2023-11-01 15:43:29 +0100

    Add quantile plot and add second dataset to DT plots

--- federated_private_decision_tree.py
+++ federated_private_decision_tree.py
@@ -2,13 +2,20 @@ import numpy as np
 import federated_private_quantiles as fpq
 
 class FederatedPrivateDecisionTree:
-    def __init__(self, max_depth=5, min_samples_split=2, num_quantiles=10, privacy_budget=1, diff_privacy = True, leaf_mechanism='laplace'):
+    def __init__(self, max_depth=5, 
+                 min_samples_split=2, 
+                 num_quantiles=10, 
+                 privacy_budget=1, 
+                 diff_privacy = True, 
+                 leaf_mechanism='laplace', 
+                 node_mechanism='laplace'):
         self.max_depth = max_depth
         self.min_samples_split = min_samples_split
         self.num_quantiles = num_quantiles
         self.privacy_budget = privacy_budget
         self.diff_privacy = diff_privacy
         self.leaf_mechanism = leaf_mechanism
+        self.node_mechanism = node_mechanism
         self.tree = {}
         self.quantiles = {}
 
@@ -30,6 +37,11 @@ class FederatedPrivateDecisionTree:
         # Select num_quantiles quantile probabilities from the possible quantile probabilities in an equidistant manner
         quantile_probabilities = quantile_probabilities[::int(n_samples / self.num_quantiles)]
         for feature in range(n_features):
+            # Check if feature is binary, hence has only 2 values (categorical feature)
+            if len(np.unique(X[:, feature])) == 2:
+                # make quantile as middlepoint between the two values
+                self.quantiles[feature] = np.array([np.mean(np.unique(X[:, feature]))])
+                continue
             self.quantiles[feature] = fpq.FederatedPrivateQuantiles(quantile_probabilities, X[:, feature], n_samples)
             self.quantiles[feature] = self.quantiles[feature].compute_quantiles().z
         return 
@@ -62,9 +74,9 @@ class FederatedPrivateDecisionTree:
         # Bin the feature into a histogram according to the quantiles and store the bin counts in each bin
         feature_binned = np.digitize(X[:, feature], bins=self.quantiles[feature])
         feature_bincounts = np.bincount(feature_binned)
-        if (self.diff_privacy == False):
+        if (self.diff_privacy == False or self.node_mechanism == 'exponential_mechanism'):
             return feature_bincounts
-        if (self.diff_privacy == True):
+        if (self.node_mechanism == 'laplace'):
             # Add laplace noise to each bincount of the histogram
             feature_bincounts = np.array([np.random.laplace(x, 1 / self.node_epsilon) for x in feature_bincounts])
             return feature_bincounts
@@ -74,16 +86,24 @@ class FederatedPrivateDecisionTree:
         best_entropy = np.inf
         best_feature = None
         best_threshold = None
+        # create matrix to store entropy for each split
+        entropy_matrix = np.zeros((n_features, self.num_quantiles))
+        threshold_counter = -1
         for feature in range(n_features):
-            # Bin the feature according to the quantiles
+            # Bin the numerical feature according to the quantiles. 
             feature_bincounts = self.__quantile_binning(X, feature)
             for threshold in self.quantiles[feature]:
+                threshold_counter += 1
                 # Calculate how many points are left of the threshold using the binned feature and round to closest integer
                 left = round(np.sum(feature_bincounts[:np.digitize(threshold, bins=self.quantiles[feature])]))
                 if left <= 0: # Go to next iteration, no split here (all points are on the right side of the threshold)
+                    if self.node_mechanism == 'exponential':
+                        entropy_matrix[feature, threshold_counter] = np.inf
                     continue
                 right = round(np.sum(feature_bincounts[np.digitize(threshold, self.quantiles[feature]):]))
                 if right <= 0: # Go to next iteration, no split here (all points are on the left side of the threshold)
+                    if self.node_mechanism == 'exponential':
+                        entropy_matrix[feature, threshold_counter] = np.inf
                     continue
 
                 # Calculate the proportions of the labels in the left and right split
@@ -101,12 +121,25 @@ class FederatedPrivateDecisionTree:
                 # If all points are on one side of the threshold, don't consider this split (maybe add a different solution later with min_leaf_size) and directly 
                 # create a leaf when this happens (in the grow_tree function)
                 if np.sum(X[:, feature] < threshold) == 0 or np.sum(X[:, feature] >= threshold) == 0:
+                    if self.node_mechanism == 'exponential':
+                        entropy_matrix[feature, threshold_counter] = np.inf
                     continue
+                
+                if self.node_mechanism == 'exponential':
+                    # Add entropy to entropy matrix
+                    entropy_matrix[feature, threshold_counter] = entropy
 
-                if entropy < best_entropy:
-                    best_entropy = entropy
-                    best_feature = feature
-                    best_threshold = threshold  
+                if self.node_mechanism == 'laplace':
+                    if entropy < best_entropy:
+                        best_entropy = entropy
+                        best_feature = feature
+                        best_threshold = threshold  
+        if self.node_mechanism == 'exponential':
+            # Find best split according to exponential mechanism with score function as negative entropy
+            scores = -entropy_matrix
+            # Calculate sensitivity
+            # TBD
+
         return best_entropy, best_feature, best_threshold
 
 
--- quantile_test.py
+++ quantile_test.py
@@ -3,33 +3,39 @@ from federated_private_quantiles import FederatedPrivateQuantiles
 import numpy as np
 from matplotlib import pyplot as plt
 
+def simulation_plot(distributions, names, max_depths, q):
+    for x in distributions:
+        diffs = []
+        for max_depth in max_depths:
+            quantile_calculator = FederatedPrivateQuantiles(q, x, max_depth=max_depth)
+            quantile_calculator.compute_quantiles()
+            empirical_quantiles = np.quantile(x, q)
+            diff = np.sum(np.abs(empirical_quantiles - quantile_calculator.z))
+            diffs.append(diff)
+        # plot the difference between compuded quantiles and empirical quantiles with max_depth on x-axis
+        plt.plot(max_depths, diffs, '-')
+    plt.title("Sum of differences between computed quantiles and empirical quantiles")
+    plt.xlabel("max_depth")
+    plt.ylabel("sum of differences")
+    plt.suptitle("n = " + str(n) + ", num_quantiles = " + str(num_quantiles))
+    plt.legend(names)
+    plt.savefig("plots/quantile_errors.png")
+
 # Example usage:
-n = 1000
+n = 100
 num_quantiles = 10
-x = np.random.poisson(10, n)
-q = np.array([i / len(x) for i in range(0, len(x), int(len(x) / num_quantiles))])
-max_depth = n
-quantile_calculator = FederatedPrivateQuantiles(q, x, max_depth=max_depth)
-sys.setrecursionlimit(max_depth + 10)
-quantile_calculator.compute_quantiles()
-print(quantile_calculator.z)
-print(quantile_calculator.left_points_counter)
-
-empirical_quantiles = np.quantile(x, q)
-# plot the difference between compuded quantiles and empirical quantiles with max_depth on x-axis
+q = np.array([i / n for i in range(0, n, int(n / num_quantiles))])
+x_poisson = np.random.poisson(10, n)
+x_gaussian = np.random.normal(0, 1, n)
+x_laplace = np.random.laplace(0, 1, n)
+x_cauchy = np.random.standard_cauchy(n)
+x_logistic = np.random.logistic(0, 1, n)
+distributions = [x_poisson, x_gaussian, x_laplace, x_cauchy, x_logistic]
+names = ["poisson", "gaussian", "laplace", "cauchy", "logistic"]
 max_depths = [i for i in range(1, 100)]
-diffs = []
-for max_depth in max_depths:
-    quantile_calculator = FederatedPrivateQuantiles(q, x, max_depth=max_depth)
-    sys.setrecursionlimit(max_depth + 10)
-    quantile_calculator.compute_quantiles()
-    diff = np.sum(np.abs(empirical_quantiles - quantile_calculator.z))
-    diffs.append(diff)
-
-plt.plot(max_depths, diffs, 'x')
-plt.title("Sum of differences between computed quantiles and empirical quantiles")
-plt.xlabel("max_depth")
-plt.ylabel("sum of differences")
-plt.suptitle("n = " + str(n) + ", num_quantiles = " + str(num_quantiles))
+sys.setrecursionlimit(100 + 10)
+simulation_plot(distributions, names, max_depths, q)
 plt.show()
+
+
 
